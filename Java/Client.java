ca4fbee61cd71718a759883bd13596dbd521a811
Wyatt Steere <94156893+wyattsteere@users.noreply.github.com>
2023-05-02 19:50:28 -0700

    added files

--- Java/Client.java
+++ Java/Client.java
@@ -1,4 +1,12 @@
+package org.example;
+
+import java.net.*;
+import java.io.*;
+
 public class Client {
+    private static Socket socket;
+    private static ObjectOutputStream out;
+    private static ObjectInputStream in;
 
     /**
      * This method name and parameters must remain as-is
@@ -19,16 +27,55 @@ public class Client {
         return "";
     }
 
+
+    private static void sendRequest(String methodName, Object[] args) throws Exception {
+        out.writeObject(methodName);
+        out.writeObject(args);
+        out.flush();
+
+        Object result = in.readObject();
+        System.out.println("Result of " + methodName + ": " + result);
+    }
+
     // Do not modify any code below this line
     // --------------------------------------
     String server = "localhost";
     public static final int PORT = 10314;
 
     public static void main(String... args) {
+        try {
+            socket = new Socket("localhost", 10314);
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Connected to server on port 10314...");
+
+
+            //RemoteMethod add = new RemoteMethod("add", new Object[]{ (Object) 2, (Object) 4});
+           // OutputStream os = socket.getOutputStream();
+           // ObjectOutputStream oos = new ObjectOutputStream(os);
+           // oos.writeObject(add);
+           // oos.flush();
+
+            sendRequest("add", new Object[]{ (Object) 2, (Object) 4});
+            sendRequest("divide", new Object[]{ (Object)10, (Object) 2});
+            sendRequest("echo", new Object[]{"Hello"});
+
+
+
+            in.close();
+            out.close();
+            socket.close();
+            System.out.println("Disconnected from server.");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+
         // All of the code below this line must be uncommented
         // to be successfully graded.
         System.out.print("Testing... ");
-
+        System.out.println(add(2,4));
         if (add(2, 4) == 6)
             System.out.print(".");
         else
@@ -46,7 +93,7 @@ public class Client {
             System.out.print(".");
         else
             System.out.print("X");
-        
+
         System.out.println(" Finished");
     }
 }
--- Java/Server.java
+++ Java/Server.java
@@ -1,14 +1,68 @@
+package  org.example;
+
+import java.net.*;
+
+import java.io.*;
+
 public class Server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket(10314);
+        System.out.println("Server listening on port 10314...");
+
+        while (true) {
+            Socket socket = server.accept();
+            System.out.println("Client connected: " + socket);
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+
+            try {
+                while (true) {
+
+                    String methodName = (String) in.readObject();
+                    Object[] argsArray = (Object[]) in.readObject();
+                    System.out.println(methodName);
+
+                    Object result;
+                    switch (methodName) {
+                        case "add":
+                            result = add((int) argsArray[0], (int) argsArray[1]);
+                            break;
+                        case "divide":
+                            result = divide((int) argsArray[0], (int) argsArray[1]);
+                            break;
+                        case "echo":
+                            String message = (String) argsArray[0];
+                            result = echo(message);
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Unknown method: " + methodName);
+                    }
+                    out.writeObject(result);
+                    out.flush();
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            } finally {
+                in.close();
+                out.close();
+                socket.close();
+                System.out.println("Client disconnected.");
+            }
+        }
+    }
 
     // Do not modify any code below tihs line
     // --------------------------------------
-    public static String echo(String message) { 
+    public static String echo(String message) {
+        System.out.println(message);
         return "You said " + message + "!";
     }
     public static int add(int lhs, int rhs) {
+        System.out.println(lhs + rhs);
         return lhs + rhs;
     }
     public static int divide(int num, int denom) {
+        System.out.println(num / denom);
         if (denom == 0)
             throw new ArithmeticException();
 
